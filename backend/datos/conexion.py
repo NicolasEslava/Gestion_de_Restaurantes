0082772711c61640e8a92002a909f0790c5dda8c
nicxls18 <erick1803duran@gmail.com>
2026-09-14 01:42:45 -0500

    Configura conexion segura con MySQL

--- backend/datos/conexion.py
+++ backend/datos/conexion.py
@@ -1,6 +1,6 @@
+import os
 import mysql.connector
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
@@ -10,7 +10,8 @@ def conectar():
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 3306))
     )
 
     return conexion
